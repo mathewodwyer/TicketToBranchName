51d00b7415d27ce2b0466dc4bdf17b2d50e7b565
mathewodwyer <mathew.odwyer@interfisystems.com>
2022-02-02 09:33:31 +1100

    Starteded

---
+++ TicketToBranchName/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace TicketToBranchName
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            Console.WriteLine("Doing the thing...");
+
+            string text = Clipboard.GetText();
+
+            // All non alphanumeric characters except a dash and space.
+            Regex regex = new Regex("[^a-zA-Z0-9 -]");
+
+            // Place dashes where whitespace characters are.
+            string result = regex.Replace(text, "").Replace(" ", "-");
+
+            Clipboard.SetText(result);
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
